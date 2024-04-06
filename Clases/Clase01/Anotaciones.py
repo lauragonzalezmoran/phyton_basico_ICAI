7299bdff045356c6b428cfedbf65add71b4a5864
Farolito123 <148919574+Farolito123@users.noreply.github.com>
2024-04-05 20:30:14 -0600

    Create Anotaciones.py

---
+++ Clases/Clase01/Anotaciones.py
@@ -0,0 +1,29 @@
+#comentarios
+
+"""comentarios"""
+
+"""
+comentarios
+multilinea
+
+"""
+
+print ('Hola'); # esto es un mensaje de bienvenida
+print ("""Linea 1  
+       Linea 2
+        Linea 3  
+""")# comentario multilinea
+
+
+linea2='\n Programa clase 1'
+linea1='\n Programa clase 1'
+print (linea2 + 'xxx')
+print (linea2 , 'xxx')
+
+numero=122
+numero2=233.4
+bandera= True
+print (type(numero))
+print (type(linea))
+print (type(bandera))
+print (type(numero2))
