b4ea0fd4894c7f24217efc65a20acf37f9033663
orsaac <orsaac@gmail.com>
2024-04-05 20:30:03 -0600

    Update Clase01.py

--- Estudiantes/Orlando_Salazar/Clase01.py
+++ Estudiantes/Orlando_Salazar/Clase01.py
@@ -1,8 +1,28 @@
+""" DocStrings
+    Esto es un comentario de multiples lineas
+    Código: estos son los ejemplos vistos en la clase 1.
+    Autor: Andrés Mena A.
 """
-Esto es un comentario 
-multilinea
 
-"""
+linea2 = '\nPrograma Clase 1' # \n caracter especial que representa un salto de linea
+print('Bienvenidos al curso de Python' + linea2) # Esto es un mensaje de bienvenida
 
-linea2 = "\nPrograma Clase 1" # Variable linea2
-print("Bienvenidos al curso de Python" + linea2) # Muestra el mensaje en pantalla
+print("Hola",linea2)
+
+mensaje = "Hola mundo desde Python"
+numero = 2024
+version = 2.1
+visible = False
+
+print(mensaje,numero,version,visible)
+
+print(type(mensaje))
+print(type(numero))
+print(type(version))
+print(type(visible))
+
+
+Nombre = input("Cual es su nombre? : ")
+Edad = input("Cual es su edad: ")
+
+print(f"Bienvenido {Nombre}, su edad es {Edad}")
