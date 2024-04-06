cb837d6799138aedc2eb4014ae6789357d7f2483
jucariel <165973607+jucariel@users.noreply.github.com>
2024-04-05 21:22:41 -0600

    Prueba1

--- Clases/Clase01/clase01.py
+++ Clases/Clase01/clase01.py
@@ -1,3 +1,8 @@
-print('Bienvenidos a Python Basico')
-linea2 = '\n Programa de la clase 1'
-print('Grupo 1 de Python' + linea2 )
+""" DocStrings
+        Esto es un comentario de multiples líneas
+        Código: estos son los ejemplos vistos en la calse 1.
+        Autor: Juan Carlos Rivera E.
+"""
+
+linea2 = '\n Programa Clase 1'  # \n caracter especial que representa un cambio de línea
+print('Bienvenidos al curso de Python' + linea2) # Esto es un mensaje de bienvenida
