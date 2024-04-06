5fcec075e917af6a634265d579fa5438ccab6e45
orsaac <orsaac@gmail.com>
2024-04-06 11:12:04 -0600

    Practica1-2

--- Estudiantes/Orlando_Salazar/Practica1.py
+++ Estudiantes/Orlando_Salazar/Practica1.py
@@ -29,3 +29,30 @@ GradosF = (float(GradosC) * 1.8) + 32
 Resp_redondeada = str(round(GradosF, 2))
 
 print(f"Los grados fahrenheit son: {Resp_redondeada}")
+
+
+#Marathon Time Calucultor - www.101computing.net/marathon
+
+#Step 1: INPUT: Ask the runner for their pace (e.g. 5:25)
+str_pace = input("At what pace do you run a km? (e.g. 5:21): ")
+
+#Step 2: PROCESS: Convert this input into a number of seconds: e.g. 5:25 = 5 * 60 + 25 = 325 seconds
+#...
+
+minutes = int(str_pace[:1])
+
+seconds = int(str_pace[-2:])
+
+total_secs = (minutes * 60) + seconds
+
+#Step 3: PROCESS: Multiply this total by 42 as there are 42km in a Marathon
+#...
+totalTime = total_secs * 42
+
+#Step 4: PROCESS: Convert this new total using the hh:mm:ss format. (e.g 3:47:30)
+#...
+
+
+#Step 5: OUTPUT: Display this new total on screen (using the hh:mm:ss format). (e.g 3:47:30)
+#...
+print(f"The total time is: {totalTime}")
--- Estudiantes/Orlando_Salazar/tempCodeRunnerFile.py
+++ Estudiantes/Orlando_Salazar/tempCodeRunnerFile.py
@@ -1,7 +1,25 @@
-GradosC = input("Digite los grados centigrados que desea convertir a fahrenheit: ")
+#Marathon Time Calucultor - www.101computing.net/marathon
 
-GradosF = (float(GradosC) * 1.8) + 32
+#Step 1: INPUT: Ask the runner for their pace (e.g. 5:25)
+str_pace = input("At what pace do you run a km? (e.g. 5:21): ")
 
-Resp_redondeada = str(round(GradosF, 2))
+#Step 2: PROCESS: Convert this input into a number of seconds: e.g. 5:25 = 5 * 60 + 25 = 325 seconds
+#...
 
-print(f"Los grados fahrenheit son: {Resp_redondeada}")
+minutes = int(str_pace[:1])
+
+seconds = int(str_pace[-2:])
+
+total_secs = (minutes * 60) + seconds
+
+#Step 3: PROCESS: Multiply this total by 42 as there are 42km in a Marathon
+#...
+totalTime = total_secs * 42
+
+#Step 4: PROCESS: Convert this new total using the hh:mm:ss format. (e.g 3:47:30)
+#...
+
+
+#Step 5: OUTPUT: Display this new total on screen (using the hh:mm:ss format). (e.g 3:47:30)
+#...
+print(f"The total time is: {totalTime}")
