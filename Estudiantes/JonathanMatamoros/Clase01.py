ef28a2f15ba4a6587d23ef462c89de73299af839
JMATAMOROS2024 <jmatamoros@bp.fi.cr>
2024-04-05 19:25:18 -0600

    Create Clase01.py
    Primer código Python

---
+++ Estudiantes/JonathanMatamoros/Clase01.py
@@ -0,0 +1,8 @@
+""" Docstrings
+    Esto es un comentario de multiples lineas
+    Código:estos son ejemplos vistos en clase 1
+    Autor: Jonathan Matamoros
+"""
+
+linea2 = '\n Programa Clase 1' # \n Programa de la clase 1)
+print('Bienvenidos a clase de Python' + linea2) # mensaje bienvenida
