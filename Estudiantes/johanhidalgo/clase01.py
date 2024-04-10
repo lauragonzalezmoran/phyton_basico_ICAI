faa2b7b1aeef2c36b359872deb7285f41331080f
ajmenaa <165622283+ajmenaa@users.noreply.github.com>
2024-04-10 17:36:31 -0600

    Squashed commit of the following:
    commit dee9bdb04fe6300bb78c0199e50c5cdbbc57bbbd
    Author: jomahs <165974818+jomahs@users.noreply.github.com>
    Date:   Wed Apr 10 17:19:05 2024 -0600
    
        Create Tarea1.py
    
        Resolucion tarea
    
    commit c7274c2fccae1b3e03bf34f53fbd7d646b5dc315
    Author: jomahs <165974818+jomahs@users.noreply.github.com>
    Date:   Wed Apr 10 01:41:47 2024 -0600
    
        Create Ejercicio.py
    
        Practica quiz
    
    commit 0ac5f187386b9b8ff6c3baa09e57e1c730ef2e6e
    Author: jomahs <165974818+jomahs@users.noreply.github.com>
    Date:   Wed Apr 10 01:41:34 2024 -0600
    
        Create tempCodeRunnerFile.py
    
    commit 0e1501edf0d503ca52e92e122de3cf43a564604e
    Author: jomahs <165974818+jomahs@users.noreply.github.com>
    Date:   Fri Apr 5 21:15:49 2024 -0600
    
        Update clase01.py
    
        Clase 2
    
    commit 8509f92c7d8ecb930d60484a2f6df5b8229060d3
    Author: jomahs <165974818+jomahs@users.noreply.github.com>
    Date:   Fri Apr 5 20:59:53 2024 -0600
    
        Update clase01.py
    
        Seg_clase_02
    
    commit 835737afa101834595b892d0d1093893f755b912
    Author: jomahs <165974818+jomahs@users.noreply.github.com>
    Date:   Fri Apr 5 20:31:17 2024 -0600
    
        Update clase01.py
    
        Avance clase 2
    
    commit 9bbc24e0ca8916c6c170d7b70fb22f33f2135ff9
    Author: jomahs <165974818+jomahs@users.noreply.github.com>
    Date:   Wed Apr 3 21:22:29 2024 -0600
    
        Create clase01.py
    
        Avance clase 01

---
+++ Estudiantes/johanhidalgo/clase01.py
@@ -0,0 +1,79 @@
+print('Bienvenidos a Python Basico')
+linea2 = '\n programa de la clase 1'
+print('Grupo1' + linea2)
+
+"""" Docs Strings
+Esto es un comentario de multiples lineas 
+Código estos son los ejemplos visto en la clase 1
+Autor: Johan Hidalgo
+"""
+linea2='\n Programa Clase 1' # Esto es un caracter especial 
+print('Bienvenidos al curso de Python'+ linea2)
+
+print('Hola', linea2)
+
+# Trabajar con diferentes tipos de datos en Python
+#Tipado dinamico: El define que tipo de dato, decimal es flotante
+# Tres reglas para asignar variables son en minusculas, no pueden empezar con numeros y no pueden ser palabras reservadas
+# Asignación de tipos de variables los espacios representan Jerarquias 
+
+mensaje = 'Hola mundo desde python'
+numero = 2024
+version = 2,1 # Variable tipo float
+visible = False
+Visible = True
+print(visible,Visible)
+print(mensaje,numero,version,visible,Visible)
+
+#Asignación en tupla
+mensaje,numero,version,visible ='Hola en python', 30, 1.1, True
+
+print(type(mensaje))
+print(type(numero))
+print(type(version))
+print(type(visible))
+
+#Cambio de inferencia de tipo 
+mensaje = 20
+print(type(mensaje))
+# Mensajes deben ser cortos
+#nombre_completo
+#lista_compras 
+#coordenadas_x
+
+#Leer datos > input 
+
+nombre = input('¿Cual es su nombre?')
+edad = input('Ingrese su edad:')
+print(f"Bienvenido:{nombre},su edad es:{edad}")
+print("Bienvenido:",nombre, "su edad es:", edad)
+
+""" Operadores
+"""
+
+#Suma
+suma = 5 + 3 
+print(f"La suma es: {suma}")
+
+#Resta
+resta = 5 - 3 
+print(f"La resta es: {resta}")
+
+#Multiplicación
+multiplicacion = 5 * 3 
+print(f"La multiplicación es: {multiplicacion}")
+
+# Division
+division = 25/5
+print(f"La division es: {division}")
+
+#División entera 
+division_entera = 10//3
+print(f"La division es: {division_entera}")
+
+#División modular 
+modulo = 33 % 2
+print(f"El modulo es: {modulo}")
+
+
+
