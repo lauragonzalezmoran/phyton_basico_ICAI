0ca841aa8eb6e9519322c3131880c13089b7ea70
jomahs <165974818+jomahs@users.noreply.github.com>
2024-04-10 19:15:19 -0600

    Clase 2
    Actualización

---
+++ Estudiantes/johanhidalgo/clase_02.py
@@ -0,0 +1,29 @@
+# Operador de igualdad me permite validadar 
+x=10
+y=10
+print(x==y) # True
+
+#Operador de distinto a !=
+x = 10
+y = 11
+print(x != y) #True
+
+#Menor que 
+x = 10
+y = 11
+print(x < y) #True
+
+#Menor igual que
+x = 10
+y = 10
+print(x <= y) #True
+
+#Mayor que
+x = 10
+y = 15
+print(x > y) #True
+
+#Mayor igual que
+x = 10
+y = 15
+print(x >= y) #True
