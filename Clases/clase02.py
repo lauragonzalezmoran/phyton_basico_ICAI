8a871a5369945b283cac95dd163bd6669ca0ff16
ajmenaa <165622283+ajmenaa@users.noreply.github.com>
2024-04-10 21:27:07 -0600

    Clase 2, ClassBook

---
+++ Clases/clase02.py
@@ -0,0 +1,37 @@
+#Operador == (igualdad)
+x = 10
+y = 10
+print(x == y)  # True
+
+#Operador != (distinto a)
+x = 10
+y = 11
+print(x != y) # True
+
+#Operador < (Menor que)
+x = 5
+y = 10
+print(x < y)# True 
+
+#Operador <= (menor a igual que)
+x = 10
+y = 10
+print(x <= y) # True
+
+#Operador > (mayor que)
+x = 15
+y = 10
+print(x > y) # True
+
+#Operador >= (mayor o igual)
+x = 15
+y = 15
+print(x >= y) # True
+
+a = 'Hola'
+b = 'hola'
+
+print(a == b) 
+
+
+
