028fedd476e596d85cdcf32b3d2bf60b74395714
ajmenaa <165622283+ajmenaa@users.noreply.github.com>
2024-04-10 22:19:58 -0600

    Squashed commit of the following:
    commit e29bbf03c49bd64a65c131c1b970d304a263cb88
    Author: oscarumana <oscarumanafernandez@gmail.com>
    Date:   Wed Apr 10 21:05:56 2024 -0600
    
        clase10 abril
    
        clase de abril 10
    
    commit 1c4f0fc06a46af639aa5e82399fa6ff244ea11a1
    Author: oscarumana <oscarumanafernandez@gmail.com>
    Date:   Fri Apr 5 20:31:46 2024 -0600
    
        Inicio
    
        Inicio

---
+++ Estudiantes/Oscar_Umana/Clase02.py
@@ -0,0 +1,26 @@
+#operador == igualdad
+x = 5
+y = 5
+print(x == y)
+
+# Operador diferente !=
+x = 5
+y = 6
+print(x != y)
+#Operador menor que <
+x = 5
+y = 10
+print(x < y)
+#Operador menor o igual que <=
+x = 5
+y = 10
+print(x <= y)
+#Operador mayor >
+x = 5
+y = 10
+print(x > y)
+
+#Operador mayor igual que 
+x = 5
+y = 10
+print(x >= y)
