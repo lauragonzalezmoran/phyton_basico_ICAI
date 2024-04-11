35aae5a5bc0afe7b4744723ebc4c4445eb705084
ajmenaa <165622283+ajmenaa@users.noreply.github.com>
2024-04-10 22:19:31 -0600

    Squashed commit of the following:
    commit 79cd3bd553105f91e028bd1ec7d25ed02ddba0c6
    Author: JohanRF07 <166186662+JohanRF07@users.noreply.github.com>
    Date:   Wed Apr 10 21:18:55 2024 -0600
    
        Create tempCodeRunnerFile.py
    
    commit 48d41bb1c9318b3d4ad429528cebb3267794d2d4
    Author: JohanRF07 <166186662+JohanRF07@users.noreply.github.com>
    Date:   Wed Apr 10 21:18:50 2024 -0600
    
        Create Estructuras_Datos.ipynb
    
    commit f8fd24facd7c7e4639da2d40a3f61c987dab21ee
    Author: JohanRF07 <166186662+JohanRF07@users.noreply.github.com>
    Date:   Wed Apr 10 21:18:43 2024 -0600
    
        Create clase02.py
    
    commit ae927aa127b88203b1f6c9f5652a7863336d295e
    Author: JohanRF07 <166186662+JohanRF07@users.noreply.github.com>
    Date:   Wed Apr 10 18:39:00 2024 -0600
    
        Create clase01.py

---
+++ Estudiantes/Johan_Rojas/clase01.py
@@ -0,0 +1,4 @@
+print('Bienvenidos a Python Basico')
+linea2 = '\n Programa de la clase 1'
+
+print('Grupo 1' + linea2)
---
+++ Estudiantes/Johan_Rojas/clase02.py
@@ -0,0 +1,33 @@
+#Operador == (igualdad)
+x = 10
+y = 10
+print(x == y)  # true
+
+#Operador != (distinto a)
+x = 10
+y = 10
+print(x != y)  # false
+
+#Operador < (menor que)
+x = 5
+y = 10
+print(x < y)  # true
+
+#Operador <= (menor o igual que)
+x = 10
+y = 10
+print(x <= y)  # true
+
+#Operador > (mayor que)
+x = 15
+y = 10
+print(x > y)  # true
+
+#Operador >= (mayor que)
+x = 15
+y = 15
+print(x >= y)  # false
+
+a = "Hola"
+b = "hola"
+print(a == b)
